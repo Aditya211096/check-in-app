249d79a4687b1bd5c3b8a6ad151f07535729ab6e
Aditya <aditya211096@gmail.com>
2026-07-23 09:06:09 +0530

    fix: import AuthModule in SuperAdminModule to resolve JwtAuthGuard dependency

--- checkin-be/src/modules/super-admin/super-admin.module.ts
+++ checkin-be/src/modules/super-admin/super-admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SuperAdminService } from "./super-admin.service";
 import { SuperAdminController } from "./super-admin.controller";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+  imports: [AuthModule],
   controllers: [SuperAdminController],
   providers: [SuperAdminService],
   exports: [SuperAdminService],
